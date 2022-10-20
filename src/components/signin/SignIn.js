eb3a4bd251692c788b4f08e2ea5dffd502deb6aa
brauliomariano <brauliomariano@gmail.com>
2022-10-20 15:34:50 -0300

    cambia leyendas ide ingreso y registro

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -39,41 +39,42 @@ class SignIn extends React.Component {
     render(){
         const { onRouteChange } = this.props;
         return(
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l shadow-5 mw6 center">
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                        <legend className="f1 fw6 ph0 mh0">Ingresar</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input 
-                                onChange={ this.onEmailChange } 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="email" 
-                                name="email-address"  
-                                id="email-address"/>
+                            <input
+                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                            type="email"
+                            name="email-address"
+                            id="email-address"
+                            onChange={this.onEmailChange}
+                            />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input 
-                                onChange={ this.onPasswordChange } 
-                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="password" 
-                                name="password"  
-                                id="password"/>
+                            <input
+                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                            type="password"
+                            name="password"
+                            id="password"
+                            onChange={this.onPasswordChange}
+                            />
                         </div>
                         </fieldset>
                         <div className="">
-                        <input 
+                        <input
                             onClick={this.onSubmitSignIn}
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Sign In"
-                            />
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                            type="submit"
+                            value="Identificarme"
+                        />
                         </div>
-                        <div>
-                            <p onClick={()=>onRouteChange('register')}
-                            className="f6 link dim black db pointer">Registrarse</p>
+                        <div className="lh-copy mt3">
+                        <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Registrarse</p>
                         </div>
                     </div>
                 </main>
